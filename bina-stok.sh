3e2ffbff1888e532e7142efbdedd0863ca7cc630
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2014-08-17 09:03:17 +0800

    melati v2
    Signed-off-by: Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>

--- bina-stok.sh
+++ bina-stok.sh
@@ -9,7 +9,7 @@ export CROSS_COMPILE=/opt/gcc-linaro/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1
 
 export KBUILD_BUILD_USER=najmi
 export KBUILD_BUILD_HOST="ampang"
-export LOCALVERSION="-Melati-AOSP-v1"
+export LOCALVERSION="-Melati-v2"
 
 #echo "Copy backup config..."
 #cp najmi-mako-config .config
